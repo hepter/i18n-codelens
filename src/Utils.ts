06fecd504762f37e8fdc7c63d9ebeec2b07fb69a
Mustafa Kuru <mustafa.kuru@akinon.com>
2022-03-10 23:17:07 +0300

    New configs added to more customize and calculatig new insert point algorithm improved to stack similar resource keys.

--- src/Utils.ts
+++ src/Utils.ts
@@ -71,12 +71,14 @@ export async function getLanguageResourcesFiles(clearCache = false, fileFsPath?:
 }
 
 
-async function applyEditByFilePath(filePath: string, nearestResourceKey: string, resourceKey: string, resourceData: string): Promise<void> {
+async function applyEditByFilePath(filePath: string, nearestResourceKey: string, resourceKey: string, resourceData: string, isUpdate?: boolean): Promise<void> {
 	const rawData = fs.readFileSync(filePath).toString();
 
 	const lineRegex = /([\t ]*?)(["']).*?(,?)(\r?\n)/;// 4 RegExp groups
 	const resourceLineRegex = new RegExp(`(?<=["'])${nearestResourceKey}(?=["']).*\r?(?:\n|$)`);
 	const matchGroups = rawData.match(lineRegex);
+	const isAutoSaveEnabled = vscode.workspace.getConfiguration("akinon-codelens").get("enableResourceAutoSaveInsertOrUpdate", true);
+	const isAutoFocusEnabled = vscode.workspace.getConfiguration("akinon-codelens").get("enableAutoFocusDocumentAfterAltered", false);
 
 	let newLine = `\n  "${resourceKey}": "${resourceData}",`;
 	if (matchGroups?.length == 5) { // 4 groups + 1 for the whole match
@@ -88,6 +90,10 @@ async function applyEditByFilePath(filePath: string, nearestResourceKey: string,
 		newLine = `${newLineChar}${space}${quote}${resourceKey}${quote}: ${quote}${resourceData}${quote}${comma}`;
 	}
 
+
+	// let previousPositionLine = 0;
+	// let previousPositionOffset = 0;
+
 	let positionLine = 0;
 	let positionOffset = 0;
 
@@ -96,6 +102,8 @@ async function applyEditByFilePath(filePath: string, nearestResourceKey: string,
 	for (const line of lines) {
 		const matchNearestLine = resourceLineRegex.exec(line);
 		if (matchNearestLine) {
+			// previousPositionLine = positionLine;
+			// previousPositionOffset = positionOffset;
 			positionLine = lineCounter;
 			positionOffset = line.length;
 			break;
@@ -104,42 +112,90 @@ async function applyEditByFilePath(filePath: string, nearestResourceKey: string,
 	}
 
 	const workspaceEdit = new vscode.WorkspaceEdit();
-	workspaceEdit.insert(vscode.Uri.file(filePath), new vscode.Position(positionLine, positionOffset), newLine);
+
+	if (isUpdate) {
+		workspaceEdit.replace(vscode.Uri.file(filePath),
+			new vscode.Range(
+				new vscode.Position(positionLine, 0),
+				new vscode.Position(positionLine, positionOffset)
+			),
+			newLine.replace(/\r?\n/g, ''));
+	} else {
+		workspaceEdit.insert(vscode.Uri.file(filePath), new vscode.Position(positionLine, positionOffset), newLine);
+	}
+
+	let dispose;
+	if (isAutoSaveEnabled) {
+		dispose = vscode.workspace.onDidChangeTextDocument(e => {
+			e.document.save();
+		});
+	}
+
 	await vscode.workspace.applyEdit(workspaceEdit);
+	dispose?.dispose();
 
-	positionLine++; // Current line is increased by 1
+	if (!isUpdate) {
+		positionLine++; // Current line is increased by 1
+	}
 
-	const document = await vscode.window.showTextDocument(vscode.Uri.file(filePath), {
-		preserveFocus: true,
-		preview: false,
-	});
 
-	const newLinePosition = new vscode.Position(positionLine, 0);
-	const newLinePositionEnd = new vscode.Position(positionLine, newLine.length);
-	document.revealRange(new vscode.Range(newLinePosition, newLinePositionEnd), vscode.TextEditorRevealType.InCenterIfOutsideViewport);
-	document.selection = new vscode.Selection(newLinePosition, newLinePositionEnd);
+	if (isAutoFocusEnabled) {
+		const document = await vscode.window.showTextDocument(vscode.Uri.file(filePath), {
+			preserveFocus: true,
+			preview: false,
+		});
+
+		const newLinePosition = new vscode.Position(positionLine, 0);
+		const newLinePositionEnd = new vscode.Position(positionLine, newLine.length);
+		document.revealRange(new vscode.Range(newLinePosition, newLinePositionEnd), vscode.TextEditorRevealType.InCenterIfOutsideViewport);
+		document.selection = new vscode.Selection(newLinePosition, newLinePositionEnd);
+	}
 }
 
-
-/**
- * [resourceKey]: 'Translated Data'
- */
 type TranslationData = {
 	[languageFileName: string]: string
 };
 
 
-export async function addNewLanguageTranslation(resourceKey: string, translationsData: TranslationData) {
+export async function addNewOrUpdateLanguageTranslation(resourceKey: string, modifiedTranslationsData: TranslationData, isUpdate?: boolean): Promise<void> {
 
 	const resourceList = await getLanguageResourcesFiles(true);
 	for (const resource of resourceList) {
+		const matchedTranslation = modifiedTranslationsData[resource.fileName];
+		if (matchedTranslation) {
+			let closestResourceKey = resourceKey;
+			if (!isUpdate) {
+				const newKeySections = resourceKey.split('.');
+				const resourceKeys = Object.keys(resource.keyValuePairs);
+				let matchSequenceCount = 0;
+				let partialMatchedKeys = [];
 
-		const matchedTranslation = translationsData[resource.fileName];
-		const closestResourceKey = closest(resourceKey, Object.keys(resource.keyValuePairs)) ?? resourceKey;
-		try {
-			await applyEditByFilePath(resource.path, closestResourceKey, resourceKey, matchedTranslation);
-		} catch (error) {
-			console.log(error);
+				// Find the closest key after sequentially matching the new key sections with the existing keys
+				for (const resourceKey of resourceKeys) {
+					const resourceSections = resourceKey.split('.');
+					let matchCount = 0;
+					for (let i = 0; i < newKeySections.length; i++) {
+						if (resourceSections[i] == newKeySections[i]) {
+							matchCount++;
+						} else {
+							break;
+						}
+					}
+					if (matchCount > matchSequenceCount) {
+						matchSequenceCount = matchCount;
+						partialMatchedKeys = [];
+					}
+					if (matchCount === matchSequenceCount) {
+						partialMatchedKeys.push(resourceKey);
+					}
+				}
+				closestResourceKey = closest(resourceKey, partialMatchedKeys) ?? resourceKey;
+			}
+			try {
+				await applyEditByFilePath(resource.path, closestResourceKey, resourceKey, matchedTranslation, isUpdate);
+			} catch (error) {
+				console.log(error);
+			}
 		}
 	}
 }
@@ -148,3 +204,29 @@ export async function addNewLanguageTranslation(resourceKey: string, translation
 export function capitalizeFirstLetter(str: string) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export const normalizeString = (str: string) => {
+	const charMap = {
+		ş: 's',
+		Ş: 'S',
+		ı: 'i',
+		İ: 'I',
+		ü: 'u',
+		Ü: 'U',
+		ğ: 'g',
+		Ğ: 'G',
+		ö: 'o',
+		Ö: 'O',
+		ç: 'c',
+		Ç: 'C',
+	} as { [key: string]: string };
+
+	// convert turkish chars to english chars
+	let replacedString = str.replace(/[^A-Za-z0-9\\[\] ]/g, function (a) {
+		return charMap[a] || a;
+	});
+
+	// remove all other non accepted characters
+	replacedString = replacedString.replace(/[^A-Za-z0-9\\[\] -_]/g, '');
+	return replacedString;
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import { commands, Disposable, ExtensionContext, languages } from 'vscode';
 import ActionAddLanguageResource from './actions/ActionAddLanguageResource';
+import ActionEditLanguageResource from './actions/ActionEditLanguageResource';
 import ActionEnableDisableCodeLens from './actions/ActionEnableDisableCodeLens';
 import ActionRefreshLanguageResource from './actions/ActionRefreshLanguageResource';
+import { ResourceEditCodeAction } from './codeAction/ResourceEditCodeAction';
 import { CodelensProvider } from './providers/CodelensProvider';
 import CompletionItemProvider from './providers/CompletionItemProvider';
 import { DecoratorProvider } from './providers/DecoratorProvider';
 import { HoverProvider } from './providers/HoverProvider';
 import ResourceWatcher from './watcher/ResourceWatcher';
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 let disposables: Disposable[] = [];
 
 
 export function activate(context: ExtensionContext) {
 
-    const codelensProvider = new CodelensProvider();
-    const hoverProvider = new HoverProvider();
-    const completionItemProvider = new CompletionItemProvider();
+    const codelensProvider = new CodelensProvider(context);
+    const hoverProvider = new HoverProvider(context);
+    const completionItemProvider = new CompletionItemProvider(context);
+    const codeActionsProvider = new ResourceEditCodeAction(context);
 
     disposables.push(ResourceWatcher());
 
     disposables.push(languages.registerCodeLensProvider(['javascript', 'typescript'], codelensProvider));
     disposables.push(languages.registerHoverProvider(['javascript', 'typescript'], hoverProvider));
-    disposables.push(languages.registerCompletionItemProvider(['javascript', 'typescript'], completionItemProvider, '.'));
+    disposables.push(languages.registerCompletionItemProvider(['javascript', 'typescript'], completionItemProvider, ''));
+    disposables.push(languages.registerCodeActionsProvider(['javascript', 'typescript'], codeActionsProvider));
 
 
     disposables.push(commands.registerCommand("akinon-codelens.enableCodeLens", ActionEnableDisableCodeLens(true)));
     disposables.push(commands.registerCommand("akinon-codelens.disableCodeLens", ActionEnableDisableCodeLens(false)));
     disposables.push(commands.registerCommand("akinon-codelens.refreshLanguageResources", ActionRefreshLanguageResource));
     disposables.push(commands.registerCommand("akinon-codelens.codelensActionAddLanguageResource", ActionAddLanguageResource, ''));
+    disposables.push(commands.registerCommand("akinon-codelens.codeActionEditLanguageResource", ActionEditLanguageResource, ''));
+
+
 
     context.subscriptions.push(...disposables);
 
