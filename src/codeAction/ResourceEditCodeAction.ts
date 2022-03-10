eb15f38119cf89570ae785af96dbeafa500d95ab
Mustafa Kuru <mustafa.kuru@akinon.com>
2022-03-10 23:13:06 +0300

    CodeActionProvider added that adds relevant code actions to resource keys to add or update resources

---
+++ src/codeAction/ResourceEditCodeAction.ts
@@ -0,0 +1,76 @@
+import * as vscode from 'vscode';
+import { getLanguageResourcesFiles } from '../Utils';
+
+export class ResourceEditCodeAction implements vscode.CodeActionProvider {
+
+	private regex!: RegExp;
+
+	constructor(context: vscode.ExtensionContext) {
+		this.refreshRegexFromConfig();
+		vscode.workspace.onDidChangeConfiguration((e) => {
+			if (e.affectsConfiguration("akinon-codelens.languageTranslatorRegex")) {
+				this.refreshRegexFromConfig();
+			}
+		}, null, context.subscriptions);
+	}
+
+	private refreshRegexFromConfig() {
+		const hoverRegex = vscode.workspace.getConfiguration("akinon-codelens").get("languageTranslatorRegex", "");
+		this.regex = new RegExp(hoverRegex, "g");
+	}
+
+	public static readonly providedCodeActionKinds = [
+		vscode.CodeActionKind.Refactor
+	];
+
+	public async provideCodeActions(document: vscode.TextDocument, range: vscode.Range): Promise<vscode.CodeAction[] | undefined> {
+		const codeActionList: vscode.CodeAction[] = [];
+
+		const startPosition = new vscode.Position(range.start.line, range.start.character);
+		const keyRange = document.getWordRangeAtPosition(startPosition, new RegExp(this.regex)); ///(?<=['"])[\w-_.]+(?=['"])/);
+		const resourceKey = document.getText(keyRange);
+
+		if (!range || !resourceKey) {
+			return;
+		}
+
+		const resources = await getLanguageResourcesFiles();
+		const missingResourceFileNames = [];
+		const existingResourceFileNames = [];
+		for (const resource of resources) {
+			if (!resource.keyValuePairs[resourceKey]) {
+				missingResourceFileNames.push(resource.fileName);
+			} else {
+				existingResourceFileNames.push(resource.fileName);
+			}
+		}
+
+
+		if (missingResourceFileNames.length > 0) {
+			const titleAdd = `Add all missing resources for '${resourceKey}' resource key. (${missingResourceFileNames.join(", ")})`;
+			const actionAdd = new vscode.CodeAction(titleAdd, vscode.CodeActionKind.Refactor);
+			actionAdd.command = {
+				title: titleAdd,
+				command: "akinon-codelens.codelensActionAddLanguageResource",
+				tooltip: 'This will open the input-box for every target language.',
+				arguments: [resourceKey]
+			};
+			codeActionList.push(actionAdd);
+		}
+
+		if (existingResourceFileNames.length > 0) {
+			const titleEdit = `Update all resources for '${resourceKey}' resource key. (${existingResourceFileNames.join(", ")})`;
+			const actionEdit = new vscode.CodeAction(titleEdit, vscode.CodeActionKind.Refactor);
+			actionEdit.command = {
+				title: titleEdit,
+				command: "akinon-codelens.codeActionEditLanguageResource",
+				tooltip: 'This will open the input-box for every target language.',
+				arguments: [resourceKey]
+			};
+			codeActionList.push(actionEdit);
+		}
+
+		return codeActionList;
+	}
+
+}
