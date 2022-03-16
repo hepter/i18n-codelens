6551fe35fee90fa091d0a25797b2e5746146f92c
Mustafa Kuru <mustafa.kuru@akinon.com>
2022-03-16 23:53:37 +0300

    HoverProvider added to warn unused resources to shows up on resource files

---
+++ src/providers/HoverProviderUnusedResources.ts
@@ -0,0 +1,28 @@
+import * as vscode from 'vscode';
+import { definitionProviderCache } from './DefinitionProvider';
+
+export class HoverProviderUnusedResources implements vscode.HoverProvider {
+
+	context: vscode.ExtensionContext;
+	constructor(context: vscode.ExtensionContext) {
+		this.context = context;
+	}
+
+	resourceLineRegex = /(?<=["'])(?<key>[\w\d\-_.]+?)(?=["'])/;
+	async provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+		const textRange = document.getWordRangeAtPosition(position, this.resourceLineRegex);
+
+		const key = document.getText(textRange);
+		if (key && document.uri.fsPath.endsWith(".json")) {
+
+			const locations = definitionProviderCache.get(key);
+			const locationsExceptJson = locations?.filter(location => !location.uri.fsPath.endsWith(".json"));
+			if (!locationsExceptJson?.length) {
+				const hoverText: vscode.MarkdownString = new vscode.MarkdownString();
+				hoverText.appendMarkdown(`The **'${key}'** resource key has no reference in any file. \n\n`);
+				hoverText.appendMarkdown(`Delete the resource key if you don't need it`);
+				return new vscode.Hover(hoverText);
+			}
+		}
+	}
+} 
